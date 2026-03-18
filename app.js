d220f48fa5bf88d91fffb11a02df061b339c8c6b
Luiszy77 <luis.f.oliveira33@aluno.senai.br>
2026-03-18 08:49:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ function login(){
 
 let u=document.getElementById("user").value;
 
-document.getElementById("login").style.display="none";
+document.getElementById("loginPage").style.display="none";
 document.getElementById("app").classList.remove("hidden");
 
 if(u==="CG100"){
 
-document.getElementById("btnDash").classList.remove("hidden");
-openTab("dashboard");
+document.getElementById("dashboard").classList.remove("hidden");
 carregarGrafico();
 
-}else{
+}else if(u==="FG100"){
 
+document.getElementById("menuFG").classList.remove("hidden");
 openTab("producao");
 
 }
@@ -28,14 +28,12 @@ openTab("producao");
 
 function openTab(id){
 
-document.querySelectorAll(".tab").forEach(t=>t.classList.add("hidden"));
+document.querySelectorAll(".page").forEach(p=>p.classList.add("hidden"));
 
 document.getElementById(id).classList.remove("hidden");
 
 if(id==="estoque"){
-
 carregarEstoque();
-
 }
 
 }
@@ -46,12 +44,10 @@ let p=document.getElementById("prodProd").value;
 let q=parseInt(document.getElementById("qtdProd").value);
 
 await db.collection("historico").add({
-
 tipo:"producao",
 produto:p,
 quantidade:q,
 data:new Date()
-
 });
 
 alert("Produção registrada");
@@ -64,12 +60,10 @@ let p=document.getElementById("prodVenda").value;
 let q=parseInt(document.getElementById("qtdVenda").value);
 
 await db.collection("historico").add({
-
 tipo:"venda",
 produto:p,
 quantidade:q,
 data:new Date()
-
 });
 
 alert("Venda registrada");
@@ -81,7 +75,6 @@ async function carregarEstoque(){
 let dados=await db.collection("historico").get();
 
 let estoque={};
-
 produtos.forEach(p=>estoque[p]=0);
 
 dados.forEach(doc=>{
@@ -89,13 +82,9 @@ dados.forEach(doc=>{
 let d=doc.data();
 
 if(d.tipo==="producao"){
-
 estoque[d.produto]+=d.quantidade;
-
 }else{
-
 estoque[d.produto]-=d.quantidade;
-
 }
 
 });
@@ -103,9 +92,7 @@ estoque[d.produto]-=d.quantidade;
 let html="";
 
 for(let p in estoque){
-
 html+=`<tr><td>${p}</td><td>${estoque[p]}</td></tr>`;
-
 }
 
 document.getElementById("estoqueTabela").innerHTML=html;
@@ -117,7 +104,6 @@ async function carregarGrafico(){
 let dados=await db.collection("historico").get();
 
 let estoque={};
-
 produtos.forEach(p=>estoque[p]=0);
 
 dados.forEach(doc=>{
@@ -125,32 +111,22 @@ dados.forEach(doc=>{
 let d=doc.data();
 
 if(d.tipo==="producao"){
-
 estoque[d.produto]+=d.quantidade;
-
 }else{
-
 estoque[d.produto]-=d.quantidade;
-
 }
 
 });
 
 new Chart(document.getElementById("grafico"),{
-
 type:"bar",
 data:{
-
 labels:Object.keys(estoque),
 datasets:[{
-
-label:"Estoque Atual",
+label:"Quantidade em Estoque",
 data:Object.values(estoque)
-
 }]
-
 }
-
 });
 
 }
