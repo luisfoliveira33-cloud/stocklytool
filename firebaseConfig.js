4194b40dbcaf604ff466b6c072ef502e1eb8e9f2
Luiszy77 <luis.f.oliveira33@aluno.senai.br>
2026-03-18 08:39:14 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 
 const firebaseConfig = {
 
-apiKey: "COLE_AQUI",
-authDomain: "COLE_AQUI",
-projectId: "COLE_AQUI",
-storageBucket: "COLE_AQUI",
-messagingSenderId: "COLE_AQUI",
-appId: "COLE_AQUI"
 
+apiKey: "AIzaSyBU1Kf5UYyHX_x7uYBJ-spPGEJH05exnrk",
+authDomain: "apikey-b1356.firebaseapp.com",
+projectId: "apikey-b1356",
+storageBucket: "apikey-b1356.firebasestorage.app",
+messagingSenderId: "747503856053",
+appId: "1:747503856053:web:b5c55fff9614f91fd751fc"
 };
 
 firebase.initializeApp(firebaseConfig);
